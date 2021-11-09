617a8f9210a7a813ad8ad080a70c102ecf1e4146
Хасп Элд <EldHasp@Gmail.com>
2021-11-09 09:08:50 +0300

    Сделана реализация интерфейса VM.

--- NDSCalc.ViewModel/Windows/MainWindowViewModel.cs
+++ NDSCalc.ViewModel/Windows/MainWindowViewModel.cs
@@ -1,10 +1,18 @@
-﻿namespace NDSCalc.ViewModel.Windows
-{
-    public class MainWindowViewModel : NotifyPropertyChanged
-    {
-        public MainWindowViewModel()
-        {
+﻿using NDSCalc.ViewModel.Interfaces;
+using Simplified;
 
-        }
+namespace NDSCalc.ViewModel.Windows
+{
+    public class MainWindowViewModel : BaseInpc, INdsCalculationViewModel
+    {
+        private decimal _amountWithoutNds;
+        private decimal _ndsRate;
+        private decimal _amountOfNds;
+        private decimal _totalAmount;
+
+        public decimal AmountWithoutNds { get => _amountWithoutNds; set => Set(ref _amountWithoutNds, value); }
+        public decimal NdsRate { get => _ndsRate; set => Set(ref _ndsRate, value); }
+        public decimal AmountOfNds { get => _amountOfNds; private set => Set(ref _amountOfNds, value); }
+        public decimal TotalAmount { get => _totalAmount; private set => Set(ref _totalAmount, value); }
     }
 }
